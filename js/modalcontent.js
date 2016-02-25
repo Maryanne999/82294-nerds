3f82d045b02ce227dfe284a771f2979a20dbec41
Maryanne999 <sofi.mishel2014@yandex.ru>
2016-02-25 13:02:53 +0300

    проект на защиту(25.02.2016)

---
+++ js/modalcontent.js
@@ -0,0 +1,15 @@
+var link = document.querySelector(".write-us");
+    var popup = document.querySelector(".modal-content");
+    var close = popup.querySelector(".modal-content-close");
+    var login = popup.querySelector("[name=login]");
+
+    link.addEventListener("click", function(event) {
+    	event.preventDefault();
+    	popup.classList.add("modal-content-show");
+		login.focus();
+      });
+
+      close.addEventListener("click", function(event) {
+        event.preventDefault();
+        popup.classList.remove("modal-content-show");
+      });
